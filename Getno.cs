b3ff34db48962997d65bf9429b7c09be7d8b4030
Souro2651996 <souro2651996@gmail.com>
2018-07-14 07:21:29 -0700

    Add files via upload

---
+++ Getno.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+namespace Primenum
+{
+    class Getno
+    {
+        public void show()
+        {
+            try
+            {
+                int n, i=2;
+                Console.Write("enter the number");
+                n = Convert.ToInt32(Console.ReadLine());
+                while (i<n)
+                {
+                    
+                
+                
+                    if (n % i == 0)
+                    {
+                        Console.Write("not prime");
+                        break;
+                    }
+
+                    i++;
+                    
+
+                }
+
+                if (i == n)
+                {
+                    Console.Write("prime");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
